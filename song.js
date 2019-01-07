56599e8b0b922c716f98cc50d780decef57a2c79
Richard Daniel <richiesme@gmail.com>
2019-01-07 12:39:02 +0100

    dataTypes Solutions

---
+++ song.js
@@ -0,0 +1,28 @@
+/* This is a detailed list of all characteristics and attributes of my favorite song called 'Ye' written and played by 'Damini Ebunoluwa Ogalu' popularly known as 'Burna Boy' */
+
+const songTitle = 'ye' // this is the song title
+const artist = 'Burna Boy' // artist who sang the song
+const genres = 'Afrobeat' // category of song
+const songAlbum = 'Outside' // album in which song is recorded
+const recordLabel = 'Spaceship Entertainment' // record label artist was signed
+const songProducer = 'Phantom' // producer who made the beat used in song
+const timeInSeconds = 233 // duration of song's playback
+const songsInOutsideAlbum = [ 'More Life', 'Ph City Vibration', 'Koni Baje', 'Sekkle Down', 'Heavens Gate', 'Ye', 'Giddem', 'Streets of Africa', 'Rock Your Body', 'Devil in califonia', 'Calm Down', 'Outside' ] // array which consist of all other songs in same album
+const releaseYear = {
+  YearReleased: 2018
+} // the year song was releaesed
+const backgroundInfo = {
+  BirthName: 'Damini Ogulu',
+  StageName: 'Burna Boy',
+  Born: '2 July 1991',
+  Age: 27,
+  Origin: 'Ahoada, Rivers State',
+  Country: 'Nigeria',
+  Instruments: 'Vocals',
+  YearActive: '2010 till Present',
+  Occupation: 'singer, songwriter, performer'
+
+} // other vital information about the artist
+
+console.log(releaseYear)
+console.log(backgroundInfo.BirthName)
