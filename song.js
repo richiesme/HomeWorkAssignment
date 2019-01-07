89f0834e13c3ef7338dc909d166a6783232a3143
Richard Daniel <richiesme@gmail.com>
2019-01-07 13:49:49 +0100

    changes made

--- song.js
+++ song.js
@@ -7,12 +7,25 @@ const songAlbum = 'Outside' // album in which song is recorded
 const recordLabel = 'Spaceship Entertainment' // record label artist was signed
 const songProducer = 'Phantom' // producer who made the beat used in song
 const timeInSeconds = 233 // duration of song's playback
-const songsInOutsideAlbum = [ 'More Life', 'Ph City Vibration', 'Koni Baje', 'Sekkle Down', 'Heavens Gate', 'Ye', 'Giddem', 'Streets of Africa', 'Rock Your Body', 'Devil in califonia', 'Calm Down', 'Outside' ] // array which consist of all other songs in same album
+const songsInOutsideAlbum = {
+  Track1: 'More Life',
+  Track2: 'Ph City Vibration',
+  Track3: 'Koni Baje',
+  Track4: 'Sekkle Down',
+  Track5: 'Heavens Gate',
+  Track6: 'Ye',
+  Track7: 'Giddem',
+  Track8: 'Streets of Africa',
+  Track9: 'Rock Your Body',
+  Track10: 'Devil in califonia',
+  Track11: 'Calm Down',
+  Track12: 'Outside'
+} // object which consist of all other songs in same album
 const releaseYear = {
   YearReleased: 2018
 } // the year song was releaesed
 const backgroundInfo = {
-  BirthName: 'Damini Ogulu',
+  BirthName: 'Damini Ebunoluwa Ogulu',
   StageName: 'Burna Boy',
   Born: '2 July 1991',
   Age: 27,
@@ -24,5 +37,8 @@ const backgroundInfo = {
 
 } // other vital information about the artist
 
-console.log(releaseYear)
-console.log(backgroundInfo.BirthName)
+console.log(songTitle) // to view title of song
+console.log(songsInOutsideAlbum) // to view all songs in OUTSIDE album
+console.log(songsInOutsideAlbum.Track1) // to view first song in the OUTSIDE album and use "TRACK1 through to TRACK12" to see first song through to last song respectively
+console.log(backgroundInfo.BirthName) // to view original birth name of artist
+console.log(backgroundInfo.Age) // to view real age of artist
